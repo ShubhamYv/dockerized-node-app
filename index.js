02b9013b876b66e5db8ab82d302e1119cec2ad28
ShubhamYv <shubhamyadav32100@gmail.com>
2024-09-04 00:24:54 +0530

    Added dotenv dependency and used in index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const app = express();
+
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.json({
@@ -17,7 +20,7 @@ app.get('/home', (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
